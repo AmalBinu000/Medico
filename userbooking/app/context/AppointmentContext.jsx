53c8e1df4f65a0876d904d6c8d63a849130c670d
Amal Binu <amalbinu000@gmail.com>
2025-04-29 19:35:10 +0530

    add changes

--- userbooking/app/context/AppointmentContext.jsx
+++ userbooking/app/context/AppointmentContext.jsx
@@ -97,7 +97,7 @@ export const AppointmentProvider = ({ children }) => {
     setSelectedAppointment(null);
   };
 
-  const colorClasses = ['bg-blue-500', 'bg-green-500', 'bg-purple-500', 'bg-pink-500', 'bg-orange-500'];
+  const colorClasses = ['bg-blue-500', 'bg-green-500', 'bg-purple-500', 'bg-pink-500', 'bg-orange-500','bg-red-500', 'bg-yellow-500', 'bg-teal-500', 'bg-indigo-500', 'bg-gray-500'];
 
     const doctorColorMap = Doctors.reduce((map, doctor, index) => {
     map[doctor.name] = colorClasses[index % colorClasses.length];
